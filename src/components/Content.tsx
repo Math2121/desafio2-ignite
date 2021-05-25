426ab89aa3d37ae53b73b9fc10515c1c04b0636f
Math2121 <matheusdepaula527@gmail.com>
2021-05-25 11:22:23 -0300

    add refatoring

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,30 @@
+import { useMovie } from "../hook/useMovie";
+import { MovieCard } from "./MovieCard";
+
 export function Content() {
-  // Complete aqui
+  const {movies,selectedGenre} = useMovie()
+  return (
+   
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
